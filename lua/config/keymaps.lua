dd9418863682cc2bea903ddd856ee4654c2fd116
yongjia <yjtoro@hotmail.com>
2023-06-06 01:48:52 +0800

    update keybinding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- <space> + fs to save
+map("n", "<leader>fs", "<cmd>update<cr>", { desc = "Update file" })
+map("n", "<leader>fS", "<cmd>w<cr>", { desc = "Save file" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,9 +9,21 @@ vim.g.sonokai_style = 'atlantis'
 vim.o.foldlevel = 99
 
 -- tab size set to 2 and always use spaces
-vim.o.tabstop = 2
-vim.o.extandtab = true
+vim.o.tabstop = 2 
+vim.o.extandtab = false
+vim.o.shiftwidth = 4
 
 -- nim autocomplete
 vim.g.ale_sign_error = '✘'
 vim.g.ale_sign_warning = '⚠'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
